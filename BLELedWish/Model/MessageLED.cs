99be22fde436049b5f464e77079c9e9e9dde43dc
XxGoldenbluexX <tonyg222@outlook.fr>
2023-06-07 17:10:30 +0200

    pas d'idée

--- BLELedWish/Model/MessageLED.cs
+++ BLELedWish/Model/MessageLED.cs
@@ -2,23 +2,13 @@
 
 namespace BLELedWish.Model
 {
-    public class MessageLED : ObservableObject
+    public partial class MessageLED : ObservableObject
     {
 
-        private string _message;
+        [ObservableProperty]
+        private string message;
 
-        public string Message
-        {
-            get => _message;
-            set => SetProperty(ref _message, value);
-        }
-
-        private bool _selected;
-
-        public bool Selected {
-            get => _selected;
-            set => SetProperty(ref _selected, value);
-        }
-
+        [ObservableProperty]
+        private bool selected;
     }
 }
